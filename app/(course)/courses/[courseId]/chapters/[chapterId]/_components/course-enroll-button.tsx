076d079f1a9fe97e99e594deaf4354412cb703c4
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-06-05 16:15:50 +0200

    implemented stripe

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -2,6 +2,10 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { set } from "zod";
 
 interface CourseEnrollButtonProps {
   courseId: string;
@@ -12,8 +16,28 @@ export const CourseEnrollButton = ({
   courseId,
   price,
 }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+      window.location.assign(response.data.url);
+    } catch (error) {
+      toast.error("An error occurred");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <Button className="w-full md:w-auto" size={"sm"}>
+    <Button
+      className="w-full md:w-auto"
+      size={"sm"}
+      onClick={onClick}
+      disabled={isLoading}
+    >
       Enroll for {formatPrice(price)}
     </Button>
   );
